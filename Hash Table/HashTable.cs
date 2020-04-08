fe64b0d0b11952143bac8083eeefbe4d3819a454
Влад Концевич <kontsevichv@mail.ru>
2020-04-08 22:37:32 +0300

    Hash table & Linked list & Binary tree

---
+++ Hash Table/HashTable.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hash_Table
+{
+    public class Item
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public Item(string key,string value)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(key));
+            Key = key;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+    class HashTable
+    {
+        private readonly byte _maxSize = 255;
+        private Dictionary<int, List<Item>> _items = null;
+        public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => _items?.ToList()?.AsReadOnly();
+        public HashTable()
+        {
+            _items = new Dictionary<int, List<Item>>(_maxSize);
+        }
+        public void Insert(string key,string value)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length > _maxSize)
+                throw new ArgumentException($"максимальная длинна ключа составляет {_maxSize} символов", nameof(key));
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            var item = new Item(key, value);
+            var hash = GetHash(item.Key);
+            List<Item> hashTableItem = null;
+            if (_items.ContainsKey(hash))
+            {
+                hashTableItem = _items[hash];
+                var oldElementWithKey = hashTableItem.SingleOrDefault(i => i.Key == item.Key);
+                if (oldElementWithKey != null)
+                    throw new ArgumentException($"хэш-таблица уже содержит элемент с ключом {key}. Ключ должен быть уникален", nameof(key));
+                _items[hash].Add(item);
+            }
+            else
+            {
+                hashTableItem = new List<Item> { item };
+                _items.Add(hash, hashTableItem);
+            }
+        }
+        public void Delete(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length > _maxSize)
+                throw new ArgumentException($"максимальная длинна ключа составляет {_maxSize} символов.", nameof(key));
+            var hash = GetHash(key);
+            if (!_items.ContainsKey(hash)) return;
+            var hashTableItem = _items[hash];
+            var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+            if (item != null)
+                hashTableItem.Remove(item);
+        }
+        public string Search(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+            if (key.Length > _maxSize)
+                throw new ArgumentException($"максимальная длинна ключа составляет {_maxSize} символов.", nameof(key));
+            var hash = GetHash(key);
+            if (!_items.ContainsKey(hash))
+                return null;
+            var hashTableItem = _items[hash];
+            if (hashTableItem != null)
+            {
+                var item = hashTableItem.SingleOrDefault(i => i.Key == key);
+                if (item != null)
+                    return item.Value;
+            }
+            return null;
+        }
+        private int GetHash(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            if (value.Length > _maxSize)
+                throw new ArgumentException($"максимальная длинна ключа составляет {_maxSize} символов.", nameof(value));            
+            return value.Length; 
+        }
+    }
+}
---
+++ Linked list/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Linked list")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linked list")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d49d5a0b-3a2f-42a0-9a38-4bd0796dbc97")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
