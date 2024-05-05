bb95472f3c823dbc234b43482111d5357b6a01ef
VelSkorp <ukantsevich@gmail.com>
2024-05-05 16:10:08 +0200

    Updated Binary tree

--- DataStructures/Trees/BinaryTree.cs
+++ DataStructures/Trees/BinaryTree.cs
@@ -1,116 +1,199 @@
 ﻿namespace Trees
 {
-    public class BinaryTree<T>
-        where T : IComparable
-    {
-        public BinaryTreeNode<T> RootNode { get; set; }
-        public BinaryTreeNode<T> Add(BinaryTreeNode<T> node, BinaryTreeNode<T> currentNode = null)
-        {
-            if (RootNode == null)
-            {
-                node.ParentNode = null;
-                return RootNode = node;
-            }
+	/// <summary>
+	/// Represents a binary tree data structure.
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the binary tree, which must implement the IComparable interface.</typeparam>
+	public class BinaryTree<T>
+		where T : IComparable<T>
+	{
+		/// <summary>
+		/// Gets the root node of the binary tree.
+		/// </summary>
+		public BinaryTreeNode<T> Root { get; private set; }
 
-            currentNode ??= RootNode;
-            node.ParentNode = currentNode;
+		/// <summary>
+		/// Adds a new element to the binary tree.
+		/// </summary>
+		/// <param name="data">The data to be added to the binary tree.</param>
+		/// <returns>True if the element is added successfully; otherwise, false.</returns>
+		public bool Add(T data)
+		{
+			var newNode = new BinaryTreeNode<T>(data);
 
-            int result;
+			if (Root is not null)
+			{
+				return Add(newNode, Root);
+			}
 
-            return (result = node.Data.CompareTo(currentNode.Data)) == 0 ? currentNode : result < 0 ? currentNode.LeftNode == null ?
-                (currentNode.LeftNode = node) : Add(node, currentNode.LeftNode) : currentNode.RightNode == null ?
-                (currentNode.RightNode = node) : Add(node, currentNode.RightNode);
-        }
-        public BinaryTreeNode<T> Add(T data)
-        {
-            return Add(new BinaryTreeNode<T>(data));
-        }
-        public BinaryTreeNode<T> FindeNode(T data, BinaryTreeNode<T> startWithNode = null)
-        {
-            startWithNode ??= RootNode;
+			Root = newNode;
+			return true;
+		}
 
-            int result;
+		/// <summary>
+		/// Finds a node with the specified value in the binary tree.
+		/// </summary>
+		/// <param name="value">The value to search for in the binary tree.</param>
+		/// <returns>The node with the specified value if found; otherwise, null.</returns>
+		/// <exception cref="InvalidOperationException">Thrown when the Binary tree is empty.</exception>
+		public BinaryTreeNode<T> FindNode(T value)
+		{
+			if (Root is null)
+			{
+				throw new InvalidOperationException("Binary tree is empty");
+			}
 
-            return (result = data.CompareTo(startWithNode.Data)) == 0 ? startWithNode : result < 0 ? startWithNode.LeftNode == null ?
-                null : FindeNode(data, startWithNode.LeftNode) : startWithNode.RightNode == null ? null : FindeNode(data, startWithNode.RightNode);
-        }
-        public void Remove(BinaryTreeNode<T> node)
-        {
-            if (node == null) return;
+			return FindNode(value, Root);
+		}
 
-            var currentNodeSide = node.NodeSide;
+		/// <summary>
+		/// Removes a node with the specified value from the binary tree.
+		/// </summary>
+		/// <param name="value">The value of the node to be removed.</param>
+		/// <returns>True if the node is removed successfully; otherwise, false.</returns>
+		/// <exception cref="InvalidOperationException">Thrown when the Binary tree is empty.</exception>
+		public bool Remove(T value)
+		{
+			var foundNode = FindNode(value);
+			return Remove(foundNode);
+		}
 
-            if (node.LeftNode == null && node.RightNode == null)
-                if (currentNodeSide == Side.Left)
-                    node.ParentNode.LeftNode = null;
-                else
-                    node.ParentNode.RightNode = null;
-            else if (node.LeftNode == null)
-            {
-                if (currentNodeSide == Side.Left)
-                    node.ParentNode.LeftNode = node.RightNode;
-                else
-                    node.ParentNode.RightNode = node.RightNode;
-                node.RightNode.ParentNode = node.ParentNode;
-            }
-            else if (node.RightNode == null)
-            {
-                if (currentNodeSide == Side.Left)
-                    node.ParentNode.LeftNode = node.LeftNode;
-                else
-                    node.ParentNode.RightNode = node.LeftNode;
-                node.LeftNode.ParentNode = node.ParentNode;
-            }
-            else
-                switch (currentNodeSide)
-                {
-                    case Side.Left:
-                        node.ParentNode.LeftNode = node.RightNode;
-                        node.RightNode.ParentNode = node.ParentNode;
+		/// <summary>
+		/// Recursively adds a new node to the binary tree starting from the given current node.
+		/// </summary>
+		/// <param name="newNode">The node to be added.</param>
+		/// <param name="currentNode">The current node being evaluated during insertion.</param>
+		/// <returns>True if the node is added successfully; otherwise, false.</returns>
+		private bool Add(BinaryTreeNode<T> newNode, BinaryTreeNode<T> currentNode)
+		{
+			if (newNode.Value.CompareTo(currentNode.Value) == 0)
+			{
+				return false;
+			}
 
-                        Add(node.LeftNode, node.RightNode);
-                        break;
-                    case Side.Right:
-                        node.ParentNode.RightNode = node.RightNode;
-                        node.RightNode.ParentNode = node.ParentNode;
+			if (newNode.Value.CompareTo(currentNode.Value) < 0)
+			{
+				if (currentNode.LeftNode is not null)
+				{
+					return Add(newNode, currentNode.LeftNode);
+				}
+				currentNode.SetLeftNode(newNode);
+				return true;
+			}
 
-                        Add(node.LeftNode, node.RightNode);
-                        break;
-                    default:
-                        var bufLeft = node.LeftNode;
-                        var bufRightLeft = node.RightNode.LeftNode;
-                        var bufRightRight = node.RightNode.RightNode;
+			if (currentNode.RightNode is not null)
+			{
+				return Add(newNode, currentNode.RightNode);
+			}
+			currentNode.SetRightNode(newNode);
+			return true;
+		}
 
-                        node.Data = node.RightNode.Data;
-                        node.RightNode = bufRightRight;
-                        node.LeftNode = bufRightLeft;
+		/// <summary>
+		/// Recursively searches for a node with the specified value starting from the given current node.
+		/// </summary>
+		/// <param name="value">The value to search for.</param>
+		/// <param name="currentNode">The current node being evaluated during the search.</param>
+		/// <returns>The node with the specified value if found; otherwise, null.</returns>
+		private BinaryTreeNode<T> FindNode(T value, BinaryTreeNode<T> currentNode)
+		{
+			if (currentNode is null)
+			{
+				return null;
+			}
 
-                        Add(bufLeft, node);
-                        break;
-                }
-        }
-        public void Remove(T data)
-        {
-            var foundNode = FindeNode(data);
-            Remove(foundNode);
-        }
-        public void PrintTree()
-        {
-            PrintTree(RootNode);
-        }
-        private void PrintTree(BinaryTreeNode<T> startNode, string indent = "", Side? side = null)
-        {
-            if (startNode != null)
-            {
-                var nodeSide = side == null ? "+" : side == Side.Left ? "L" : "R";
+			if (value.CompareTo(currentNode.Value) == 0)
+			{
+				return currentNode;
+			}
 
-                Console.WriteLine($"{indent} [{nodeSide}]- {startNode.Data}");
+			if (value.CompareTo(currentNode.Value) < 0)
+			{
+				return FindNode(value, currentNode.LeftNode);
+			}
 
-                indent += new string(' ', 3);
+			return FindNode(value, currentNode.RightNode);
+		}
 
-                PrintTree(startNode.LeftNode, indent, Side.Left);
-                PrintTree(startNode.RightNode, indent, Side.Right);
-            }
-        }
-    }
+		/// <summary>
+		/// Removes the specified node from the binary tree.
+		/// </summary>
+		/// <param name="node">The node to be removed.</param>
+		/// <returns>True if the node is removed successfully; otherwise, false.</returns>
+		private bool Remove(BinaryTreeNode<T> node)
+		{
+			if (node is null)
+			{
+				return false;
+			}
+
+			if (node.LeftNode is null && node.RightNode is null)
+			{
+				if (node == Root)
+				{
+					Root = null;
+				}
+				else
+				{
+					ParentSetChildNode(node, null);
+				}
+				return true;
+			}
+
+			if (node.LeftNode is null || node.RightNode is null)
+			{
+				var newChild = node.LeftNode ?? node.RightNode;
+				newChild.SetParentNode(node.ParentNode);
+				ParentSetChildNode(node, newChild);
+
+				if (node == Root)
+				{
+					Root = newChild;
+				}
+
+				return true;
+			}
+
+			var successor = FindSuccessor(node);
+			node.SetValue(successor.Value);
+			return Remove(successor);
+		}
+
+		/// <summary>
+		/// Sets the child node of a parent node to the specified child node.
+		/// </summary>
+		/// <param name="node">The parent node whose child node will be set.</param>
+		/// <param name="childNode">The child node to be set.</param>
+		private void ParentSetChildNode(BinaryTreeNode<T> node, BinaryTreeNode<T> childNode)
+		{
+			if (node.ParentNode.LeftNode == node)
+			{
+				node.ParentNode.SetLeftNode(childNode);
+			}
+			else
+			{
+				node.ParentNode.SetRightNode(childNode);
+			}
+		}
+
+		/// <summary>
+		/// Finds the successor node of the specified node in the binary tree.
+		/// </summary>
+		/// <param name="node">The node for which to find the successor.</param>
+		/// <returns>The successor node of the specified node.</returns>
+		private BinaryTreeNode<T> FindSuccessor(BinaryTreeNode<T> node)
+		{
+			if (node.RightNode == null)
+			{
+				return null;
+			}
+
+			node = node.RightNode;
+			while (node.LeftNode != null)
+			{
+				node = node.LeftNode;
+			}
+			return node;
+		}
+	}
 }
--- DataStructures/Trees/BinaryTreeNode.cs
+++ DataStructures/Trees/BinaryTreeNode.cs
@@ -1,23 +1,81 @@
-﻿using System;
-
-namespace Trees
+﻿namespace Trees
 {
-    public enum Side
-    {
-        Left,Right
-    }
+	/// <summary>
+	/// Represents a node in a binary tree.
+	/// </summary>
+	/// <typeparam name="T">The type of value stored in the node, which must implement the IComparable interface.</typeparam>
+	public class BinaryTreeNode<T>
+		where T : IComparable<T>
+	{
+		/// <summary>
+		/// Gets the value stored in the node.
+		/// </summary>
+		public T Value { get; private set; }
 
-    public class BinaryTreeNode<T> where T:IComparable
-    {
-        public T Data { get; set; }
-        public BinaryTreeNode<T> LeftNode { get; set; }
-        public BinaryTreeNode<T> RightNode { get; set; }
-        public BinaryTreeNode<T> ParentNode { get; set; }
-        public BinaryTreeNode(T data)
-        {
-            Data = data;
-        }
-        public Side? NodeSide => ParentNode == null ? (Side?)null : ParentNode.LeftNode == this ? Side.Left : Side.Right;
-        public override string ToString() => Data.ToString();
-    }
+		/// <summary>
+		/// Gets the parent node of the current node.
+		/// </summary>
+		public BinaryTreeNode<T> ParentNode { get; private set; }
+
+		/// <summary>
+		/// Gets the left child node of the current node.
+		/// </summary>
+		public BinaryTreeNode<T> LeftNode { get; private set; }
+
+		/// <summary>
+		/// Gets the right child node of the current node.
+		/// </summary>
+		public BinaryTreeNode<T> RightNode { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the BinaryTreeNode&lt;T&gt; class with the specified value.
+		/// </summary>
+		/// <param name="data">The value to be stored in the node.</param>
+		public BinaryTreeNode(T data)
+		{
+			Value = data;
+		}
+
+		/// <summary>
+		/// Sets the parent node of the current node.
+		/// </summary>
+		/// <param name="parent">The parent node to be set.</param>
+		public void SetParentNode(BinaryTreeNode<T> parent)
+		{
+			ParentNode = parent;
+		}
+
+		/// <summary>
+		/// Sets the left child node of the current node.
+		/// </summary>
+		/// <param name="left">The left child node to be set.</param>
+		public void SetLeftNode(BinaryTreeNode<T> left)
+		{
+			LeftNode = left;
+		}
+
+		/// <summary>
+		/// Sets the right child node of the current node.
+		/// </summary>
+		/// <param name="right">The right child node to be set.</param>
+		public void SetRightNode(BinaryTreeNode<T> right)
+		{
+			RightNode = right;
+		}
+
+		/// <summary>
+		/// Sets the value stored in the node.
+		/// </summary>
+		/// <param name="value">The value to be set.</param>
+		public void SetValue(T value)
+		{
+			Value = value;
+		}
+
+		/// <summary>
+		/// Returns a string representation of the node's value.
+		/// </summary>
+		/// <returns>A string representing the value stored in the node.</returns>
+		public override string ToString() => Value.ToString();
+	}
 }
